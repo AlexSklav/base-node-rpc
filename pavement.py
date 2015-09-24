a31001b612f1517049a7bcd89ee6a63f17baaa24
Ryan Fobel <ryan@fobel.net>
2015-09-24 12:45:36 -0400

    Bump arduino-rpc version

--- pavement.py
+++ pavement.py
@@ -37,7 +37,7 @@ options(
                url=URL,
                license='GPLv2',
                install_requires=['arduino_scons>=0.1.post8',
-                                 'arduino-rpc>=1.6.post17'],
+                                 'arduino-rpc>=1.6.post18'],
                # Install data listed in `MANIFEST.in`
                include_package_data=True,
                packages=[str(PROJECT_PREFIX)]))
