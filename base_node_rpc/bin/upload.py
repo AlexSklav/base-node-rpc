213d8c6a816b00b33e7315debb3de2e7ba036127
Ryan Fobel <ryan@fobel.net>
2015-10-13 13:36:11 -0400

    Use arduino-helpers upload function
    Replaces existing function from arduino-rpc  package. Also, print
    the output from avrdude to the terminal.

--- base_node_rpc/bin/upload.py
+++ base_node_rpc/bin/upload.py
@@ -1,9 +1,10 @@
-from arduino_rpc.upload import upload, parse_args
+from arduino_helpers.upload import upload, parse_args
+
 from .. import get_firmwares
 
 
 if __name__ == '__main__':
     args = parse_args()
 
-    upload(args.board_name, lambda b: get_firmwares()[b][0], args.port,
-           args.arduino_install_home, verify=not args.skip_verify)
+    print upload(args.board_name, lambda b: get_firmwares()[b][0], args.port,
+                 args.arduino_install_home, verify=not args.skip_verify)
