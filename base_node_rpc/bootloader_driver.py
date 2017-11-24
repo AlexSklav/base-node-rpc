917552c1208497879234c9fb6a54c7035a92f9c2
Christian Fobel <christian@fobel.net>
2017-11-24 11:57:43 -0500

    feat(i2c-bootloader-api): add write_firmware func
    Add `TwiBootloader.write_firmware` method to write firmware to flash
    memory from an [Intel HEX file][1].
    
    [1]: https://en.wikipedia.org/wiki/Intel_HEX

--- base_node_rpc/bootloader_driver.py
+++ base_node_rpc/bootloader_driver.py
@@ -1,7 +1,12 @@
+import itertools as it
 import struct
+import time
 import types
 
 import numpy as np
+import path_helpers as ph
+
+from .intel_hex import parse_intel_hex
 
 
 def _data_as_list(data):
@@ -147,3 +152,68 @@ class TwiBootloader(object):
         data = _data_as_list(data)
         self.proxy.i2c_write(self.bootloader_address, [0x02, 0x02, addrh,
                                                        addrl] + data)
+
+    def write_firmware(self, firmware_path):
+        '''
+        Write `Intel HEX file`__ and split into pages.
+
+        __ Intel HEX file: https://en.wikipedia.org/wiki/Intel_HEX
+
+        Parameters
+        ----------
+        firmware_path : str
+            Path of Intel HEX file to read.
+        page_size : int
+            Size of each page.
+        '''
+        chip_info = self.read_chip_info()
+
+        pages = load_pages(firmware_path, chip_info['page_size'])
+
+        for i, page_i in enumerate(pages):
+            print 'Write page: %4d/%d     \r' % (i + 1, len(pages)),
+            self.write_flash(i * chip_info['page_size'], page_i)
+            # Delay to allow bootloader to finish writing to flash.
+            time.sleep(.01)
+
+            print 'Verify page: %4d/%d    \r' % (i + 1, len(pages)),
+            # Verify written page.
+            verify_data_i = self.read_flash(i * chip_info['page_size'],
+                                            chip_info['page_size'])
+            assert((verify_data_i == page_i).all())
+            # Delay to allow bootloader to finish processing flash read.
+            time.sleep(.01)
+
+
+def load_pages(firmware_path, page_size):
+    '''
+    Load `Intel HEX file`__ and split into pages.
+
+    __ Intel HEX file: https://en.wikipedia.org/wiki/Intel_HEX
+
+    Parameters
+    ----------
+    firmware_path : str
+        Path of Intel HEX file to read.
+    page_size : int
+        Size of each page.
+
+    Returns
+    -------
+    list
+        List of page contents, where each page is represented as a list of
+        integer byte values.
+    '''
+    firmware_path = ph.path(firmware_path)
+    with firmware_path.open('r') as input_:
+        data = input_.read()
+
+    df_data = parse_intel_hex(data)
+    data_bytes = list(it.chain(*df_data.loc[df_data.record_type == 0, 'data']))
+
+    pages = [data_bytes[i:i + page_size]
+             for i in xrange(0, len(data_bytes), page_size)]
+
+    # Pad end of last page with 0xFF to fill full page size.
+    pages[-1].extend([0xFF] * (page_size - len(pages[-1])))
+    return pages
